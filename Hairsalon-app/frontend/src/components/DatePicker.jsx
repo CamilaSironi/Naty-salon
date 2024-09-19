1f427bc1f7260314f50548dafd9a953bd3ff6ebf
Camila Sironi <sironicamila98@gmail.com>
2024-09-19 16:39:32 -0300

    add forms functionality

--- Hairsalon-app/frontend/src/components/DatePicker.jsx
+++ Hairsalon-app/frontend/src/components/DatePicker.jsx
@@ -1,6 +1,8 @@
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { useState } from 'react';
 
 function DatePicker() {
@@ -26,82 +28,118 @@ function DatePicker() {
     setData({...data, [key]: value})
   }
 
-  async function handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
     
-    const text = "Me gustaría reservar un turno si tenés libre para el dia "+data.date+" de "+data.time+" "+data.message;
-    const URI = encodeURIComponent(text).replaceAll('+','%20');
-    
-    const res = await fetch("https://wa.me/+5492262551283?text=Hola%20Naty!%20"+URI, 
-      {
-        mode: 'no-cors'
-      })
-    console.log(res.status);
+    const message = "Me gustaría reservar un turno si tenés libre para el dia "+data.date+" a la "+data.time+". "+data.message;
+    const URI = encodeURIComponent(message).replaceAll('+','%20');
 
-    setData({
-      date: "",
-      time: "",
-      message:""
-    })
+    document.getElementById("ok-btn").disabled = true;
+
+    const col2 = document.getElementById('form-col-2');
+    col2.insertAdjacentHTML('afterbegin', `
+      <div id="parent">
+
+          <div id="child1">
+              <h2 className='title'>Revisá tu mensaje antes de enviarlo:</h2>
+              <p>${message}</p>
+          </div>
+
+          <div id="child2">
+              <button id="btn1" class='btn shadow'>Corregir</button>
+              <button id="btn2" class='btn shadow'><a href='https://wa.me/+5492262551283?text=Hola%20Naty!%20+${URI}' target='blank'>Enviar</a></button>
+          </div>
+
+      </div>`
+    );
+
+    const removeMessage = () => {
+      col2.removeChild(document.getElementById("parent"));
+      document.getElementById("ok-btn").disabled = false;
+    }
+
+    const sendMessage = () => {
+      removeMessage();
+      setData({
+        date: "",
+        time: "",
+        message:""
+      })
+    }
+
+    const btn1 = document.getElementById("btn1");
+    btn1.addEventListener("click", function(){
+        removeMessage()});
+
+    const btn2 = document.getElementById("btn2");
+    btn2.addEventListener("click", function(){
+        sendMessage()});
   }
 
   return (
     <Container id="turnos" className="mt-5 mb-5 p-5">
     <h1 className='title'>Agendá tu turno:</h1>
 
-    <Form onSubmit={handleSubmit}>
+        <Row>
+            <Col className="form-item">
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group 
+                      className="m-3" 
+                      //validationState={this.getValidationState()}
+                    > 
+                      <Form.Label>Elegí una fecha:</Form.Label>
+                      <Form.Control 
+                        className='form-control w-75' 
+                        type="date" 
+                        name="date"
+                        value={data.date}
+                        onChange={handleChange}
+                        required
+                      />
+                    </Form.Group>
 
-      <Form.Group 
-        className="m-3" 
-        //validationState={this.getValidationState()}
-      > 
-        <Form.Label>Elegí una fecha:</Form.Label>
-        <Form.Control 
-          className='form-control w-50' 
-          type="date" 
-          name="date"
-          value={data.date}
-          onChange={handleChange}
-        />
-      </Form.Group>
+                    <Form.Group 
+                      className="m-3" 
+                      //validationState={this.getValidationState()}
+                    >
+                        <Form.Label htmlFor="select">Elegí un horario</Form.Label>
+                        <Form.Select 
+                          className='form-control w-75' 
+                          id="select"
+                          name="time"
+                          value={data.time}
+                          onChange={handleChange}
+                          required
+                        >
+                          <option value='' disabled></option>
+                          <option value='mañana'>Mañana</option>
+                          <option value='tarde'>Tarde</option>
+                        </Form.Select>
+                      </Form.Group>
 
-      <Form.Group 
-        className="m-3" 
-        //validationState={this.getValidationState()}
-      >
-          <Form.Label htmlFor="select">Elegí un horario</Form.Label>
-          <Form.Select 
-            className='form-control w-50' 
-            id="select"
-            name="time"
-            value={data.time}
-            onChange={handleChange}
-          >
-            <option>Seleccionar horario</option>
-            <option>Mañana</option>
-            <option>Tarde</option>
-          </Form.Select>
-        </Form.Group>
+                    <Form.Group 
+                      className="m-3" 
+                      //validationState={this.getValidationState()}
+                    >
+                      <Form.Label>Dejame tu mensaje: (opcional)</Form.Label>
+                      <Form.Control 
+                        className='form-control w-75 h-75' 
+                        type="text" 
+                        name="message"
+                        value={data.message}
+                        onChange={handleChange}
+                        placeholder="Escribí tu mensaje..." 
+                      />
+                    </Form.Group>
 
-      <Form.Group 
-        className="m-3" 
-        //validationState={this.getValidationState()}
-      >
-        <Form.Label>Dejame tu mensaje:</Form.Label>
-        <Form.Control 
-          className='form-control w-50 h-75' 
-          type="text" 
-          name="message"
-          value={data.message}
-          onChange={handleChange}
-          placeholder="Escribí tu mensaje..." 
-        />
-      </Form.Group>
+                    <Button id="ok-btn" variant="primary" type="submit" className='btn shadow'>Ok</Button>
+                    </Form>
+            </Col>
 
-      <Button variant="primary" type="submit" className='btn'>
-        Enviar
-      </Button>
-    </Form>
+            <Col id="form-col-2" className="p-5"></Col>
+            </Row>
+
+    
     </Container>
   )
 }
--- Hairsalon-app/frontend/src/components/Voucher.jsx
+++ Hairsalon-app/frontend/src/components/Voucher.jsx
@@ -25,68 +25,102 @@ function Voucher() {
         setData({...data, [key]: value})
     };
 
-    async function handleSubmit(e) {
+    function handleSubmit(e) {
         e.preventDefault();
+
+        const message = "Me gustaría regalarle un voucher a "+data.to+" con el mensaje: "+data.message+". Me pasarías info sobre que tratamientos se puede hacer? Gracias!";
+        const URI = encodeURIComponent(message).replaceAll('+','%20');
+
+        const voucherElement = document.getElementById("voucher-form");
+        voucherElement.classList.toggle("hidden");
         
-        const text = "Me gustaría regalarle un voucher a "+data.to+" con el mensaje: "+data.message+" Me pasarías info sobre que tratamientos se puede hacer? Gracias!";
-        const URI = encodeURIComponent(text).replaceAll('+','%20');
-        
-        const res = await fetch("https://wa.me/+5492262551283?text=Hola%20Naty!%20"+URI, 
-        {
-            mode: 'no-cors'
-        })
-        console.log(res);
+        const col2 = document.getElementById('voucher-col-2');
+        col2.insertAdjacentHTML('afterbegin', `
+          <div id="parent">
     
-        setData({
-          to: "",
-          message:""
-        })
-      }
+              <div id="child1">
+                  <h2 className='title'>Revisá tu mensaje antes de enviarlo:</h2>
+                  <p>${message}</p>
+              </div>
+    
+              <div id="child2">
+                  <button id="btn1" class='btn shadow'>Corregir</button>
+                  <button id="btn2" class='btn shadow'><a href='https://wa.me/+5492262551283?text=Hola%20Naty!%20+${URI}' target='blank'>Enviar</a></button>
+              </div>
+    
+          </div>`
+        );
+    
+        const removeMessage = () => {
+          col2.removeChild(document.getElementById("parent"));
+          voucherElement.classList.toggle("hidden");
+        }
+    
+        const sendMessage = () => {
+          removeMessage();
+          setData({
+            to: "",
+            message:""
+          });
+        }
+    
+        const btn1 = document.getElementById("btn1");
+        btn1.addEventListener("click", function(){
+            removeMessage()});
+    
+        const btn2 = document.getElementById("btn2");
+        btn2.addEventListener("click", function(){
+            sendMessage()});
+        
+    }
 
     return(
-        <Container id="voucher" className="mt-5 mb-5 p-5 border">
+        <Container id="voucher" className="mt-5 mb-5 p-5 rounded">
             <Row>
                 <Col className="p-5">
                     <Image className='shadow mb-5 rounded' src={logo} fluid />
                 </Col>
 
-                <Col className="p-5">
-                    <h1 className='title'>Regalá un voucher!</h1>
+                <Col id="voucher-col-2" className="p-5">
+                    <div id="voucher-form">
+                        <h1 className='title'>Regalá un voucher!</h1>
 
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group 
-                            className="m-3" 
-                            //validationState={this.getValidationState()}
-                        > 
-                            <Form.Label>Para:</Form.Label>
-                            <Form.Control 
-                                className='form-control w-50' 
-                                type="text" 
-                                name="to"
-                                value={data.to}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group 
+                                className="m-3" 
+                                //validationState={this.getValidationState()}
+                            > 
+                                <Form.Label>Para:</Form.Label>
+                                <Form.Control 
+                                    className='form-control w-50' 
+                                    type="text" 
+                                    name="to"
+                                    value={data.to}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </Form.Group>
 
-                        <Form.Group 
-                            className="m-3" 
-                            //validationState={this.getValidationState()}
-                        >
-                            <Form.Label>Dejale un mensaje:</Form.Label>
-                            <Form.Control 
-                                className='form-control w-50' 
-                                type="text"
-                                name="message"
-                                value={data.message}
-                                onChange={handleChange}
+                            <Form.Group 
+                                className="m-3" 
+                                //validationState={this.getValidationState()}
                             >
-                            </Form.Control>
-                        </Form.Group>
+                                <Form.Label>Dejale un mensaje:</Form.Label>
+                                <Form.Control 
+                                    className='form-control w-50' 
+                                    type="text"
+                                    name="message"
+                                    value={data.message}
+                                    onChange={handleChange}
+                                    required
+                                >
+                                </Form.Control>
+                            </Form.Group>
 
-                    <Button variant="primary" type="submit" className='btn'>
-                        Enviar
-                    </Button>
-                    </Form>
+                        <Button variant="primary" type="submit" className='btn shadow'>Ok</Button>
+
+                        </Form>
+                    </div>
                 </Col>
             </Row>
         </Container>
